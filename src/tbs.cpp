32d94ac083acbfe1aefe05bdd1e6268612392e33
Martin Schwarz <martin.schwarz10@gmail.com>
2015-04-12 19:10:28 +0200

    added logging lib

--- src/tbs.cpp
+++ src/tbs.cpp
@@ -1,6 +1,11 @@
-#include <iostream>
+#include "tbs.h"
 
 int main(){
-    std::cout << "Hello world!" << std::endl;
+    LOG_INIT("log.txt");
+    LOG_F_TRACE(GAME_LOG_PATH, "log test trace");
+    LOG_F_INFO(GAME_LOG_PATH, "log test info");
+    LOG_F_WARNING(GAME_LOG_PATH, "log test warning");
+    LOG_F_ERROR(GAME_LOG_PATH, "log test error");
+
     return 0;
 }
--- src/tbs.h
+++ src/tbs.h
@@ -1,1 +1,5 @@
- 
+
+#include <iostream>
+#include "libs/mlog.h"
+
+#define GAME_LOG_PATH "log_game.txt"
